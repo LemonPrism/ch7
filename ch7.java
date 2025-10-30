45e1073400f2673a738617d3a3e6565c7c1caab5
LemonPrism <charliechenvan@gmail.com>
2025-10-30 14:17:52 -0700

    Add files via upload

--- ch7.java
+++ ch7.java
@@ -3,15 +3,25 @@
 public class ch7
 {
     
-    public static void main (String []args){
+    public static void main (String [] args){
         
         
-        int [] nums = { - 2 , 1 , 6 , -3 , 10 , 8};
+        int [] nums = { 5, 10, 56 , 20 };
+        int n = 5;
+        String msg = " Hello"; 
         
-        System.out.println (indexOfMax( nums)); 
+        
+        System.out.println (letterHist( msg ) ); 
+        // System.out.println (arePrimeFactors ( nums, n ) ) ;  
+        //System.out.println (areFactors ( nums, n)  );
+        //System.out.println(sieve(n)) ;
+        //System.out.println (indexOfMax( nums)); 7.3
+        
         
     }
     
+    //7.3 
+    /*
     public static int indexOfMax ( int[] nums ){
         
         
@@ -27,5 +37,138 @@ public class ch7
         return largest  ; 
         
     }
+    
+    */
    
-}
+   
+    //7.4
+    
+    /*
+    
+    
+    public static boolean [] sieve (int n){
+        
+        boolean [] sieve = new boolean[n]; 
+        
+        for ( int k = 0; k < n ; k ++ ){
+            sieve [i] = true; 
+        }
+        sieve [0] = sieve [ 1 ] = false;
+        int i = 2 ; 
+        while ( i < n ) {
+        
+         while ( sieve[i] == false ){
+             i ++ ; 
+         } 
+         int prime = i ; 
+         if( i < n ) {
+             i += prime; 
+             while ( i < n ) {
+                 sieve[i] = false; 
+             }
+              
+               i = prime + 1; 
+            }
+         
+        } 
+     
+        return sieve; 
+        
+        }
+        
+       
+        */
+                 
+        
+       
+        //7.5
+    
+        /*
+    
+        public static boolean  areFactors ( int[] nums , int n ) {
+        
+         
+        
+         int i = 0 ; 
+         
+         while ( i < nums.length ) {
+             
+             if (  nums[i]    % n  != 0  ){
+                 return false; 
+             }
+            i ++; 
+             
+         }
+         
+         
+         
+         return  true;
+        
+        
+        }
+        
+        */
+       
+       //7.6
+       /*
+       
+       public static boolean  arePrimeFactors ( int[] nums , int n ) {
+        
+         
+        
+         int i = 0 ; 
+         
+         while ( i < nums.length ) {
+             
+             if (  nums[i]    % n  != 0  ){
+                 return false; 
+             }
+            i ++; 
+             
+         }
+         
+         
+         
+         return  true;
+        
+        
+        }
+        */
+       
+       //7.7
+       
+      
+       public static int []  letterHist ( String msg ) {
+           
+           msg= msg.toLowerCase();
+           
+           int [] alphabet = new int [26] ; 
+           
+           
+           int i = 0 ; 
+           
+           while ( i < msg.length() ) {
+               
+               
+               
+               
+               
+               i ++;
+           }
+           
+           return 
+           
+           
+           
+           
+           
+           
+        
+             
+        
+         
+        
+        }
+        
+        
+    } 
